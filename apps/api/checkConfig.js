374357cfcb7d7330b8a28e444aa75e696584bc84
devzota <dev.zota@gmail.com>
2026-07-31 15:18:35 +0700

    Remove sensitive scripts from version control

--- apps/api/checkConfig.js
+++
@@ -1,9 +0,0 @@
-const { Client } = require('pg');
-async function run() {
-  const client = new Client({ connectionString: 'postgresql://root:rootpassword@localhost:5432/ztreel?schema=public' });
-  await client.connect();
-  const res = await client.query("SELECT name, auto_scan_batch_size, auto_queue_limit FROM ztteam_pages WHERE name = 'Địa điểm Đà Nẵng'");
-  console.log(res.rows);
-  await client.end();
-}
-run();
